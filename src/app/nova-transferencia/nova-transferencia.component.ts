fde1d439e0cd21224b25513756c9c7b4411bb81c
Daniel Souza <daniielsouzapvh@gmail.com>
2021-06-24 23:17:43 -0300

    feat: add formulario de transferencia

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,27 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0
+  destino: number = 0
+
+  transferir() {
+    const valorEmitir = { valor: this.valor, destino: this.destino }
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampost();
+  }
+
+  limparCampost(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
